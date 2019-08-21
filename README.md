6b7eddbe3634f5a59f23b5427d36653f6aba5d47
Iain Hunter <iain.hunter@outlook.com>
2019-08-21 21:38:29 +0100

    Add setup script and utils

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # foss4g-serverless-ops
 Repo containing Terraform scripts for spinning up AWS serverless environment
 
-#### Running the Container
+## First Run Config
+Before we run for the first time we need to do a small amount of setup explained on this wiki page - [First Run Config](https://github.com/thinkWhere/foss4g-serverless-ops/wiki/First-Run-Config) 
+
+## Running the Container
 The container can be run as follows, once logged in you MUST run the ```setenv.sh``` to ensure environment is set up correctly
 
 ```bash
@@ -12,5 +15,8 @@ cd ops
 
 ## TODO
 
-* Explain setting up AWS credentials
-* Setup an S3 bucket to hold Terraform State
+* First Run Config
+    * Set up a Terraform Admin User
+    * Setup an S3 bucket to hold Terraform State
+* Docker-compose and setenv.sh
+* 
--- ops/setenv.sh
+++ ops/setenv.sh
@@ -7,3 +7,31 @@
 REGION="us-east-1"
 PROFILE="foss4g_ops"
 BUCKET="foss4g-ops-tfstate"
+
+# Add utils into path
+export PATH=$PATH:`pwd`/utils
+
+printf "#########################################################################\n"
+printf "thinkWhere FOSS4G Serverless Ops\n\n"
+
+# printf "Clean up Windows weirdness on utils ...\n\n"
+dos2unix `pwd`/utils/tfinit
+dos2unix  ~/.aws/credentials
+dos2unix `pwd`/utils/parse_config.py
+
+printf "\nSetting Tooling vars...\n"
+
+export TF_VAR_region=$REGION
+printf "\nTerraform AWS region:       $TF_VAR_region \n"
+
+export TF_VAR_profile=$PROFILE
+printf "Terraform Local profile:    $TF_VAR_profile \n"
+
+export TF_VAR_bucket=$BUCKET
+printf "Terraform Config S3 bucket: $TF_VAR_bucket \n"
+
+printf "#########################################################################\n"
+
+# push environment onto command line - so easy to see what we're using
+# help is here https://www.digitalocean.com/community/tutorials/how-to-customize-your-bash-prompt-on-a-linux-vps
+export PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \e[0;36m[$TF_VAR_profile]\n\[\033[00m\]\$ '
