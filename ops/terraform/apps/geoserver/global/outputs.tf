58bff60327472dca2a5cf57c3ab1ec8805e961ee
Iain Hunter <iain.hunter@thinkwhere.com>
2019-08-23 15:40:57 +0100

    Setup cluster

---
+++ ops/terraform/apps/geoserver/global/outputs.tf
@@ -0,0 +1,19 @@
+output "ecs_cluster_arn" {
+  description = "ARN of the ECS cluster"
+  value       = aws_ecs_cluster.geoserver_ecs_cluster.arn
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.geoserver_ecs_cluster.name
+}
+
+output "ecs_service_role" {
+  description = "ARN of the ECS service role"
+  value       = aws_iam_role.ecs_service_role.arn
+}
+
+output "ecs_task_role" {
+  description = "ARN of the ECS task role"
+  value       = aws_iam_role.ecs_task_role.arn
+}
